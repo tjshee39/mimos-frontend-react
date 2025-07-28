f43ad45ad49cbaf96bcc497f6722994735515667
한선희 <gksh054@naver.com>
2025-07-28 17:28:50 +0900

    Refactor: post service query POST버전 추가

--- src/app/core/service/post.service.ts
+++ src/app/core/service/post.service.ts
@@ -6,10 +6,20 @@ export const postApi = api.injectEndpoints({
     getPosts: build.query<Post[], void>({
       query: () => `/post/list`,
       // providesTags: ['User'],
+      // transformResponse: (baseQueryReturnValue: CommonResponse<Post[]>) => {
+      //   return baseQueryReturnValue.response
+      // }
     }),
     getPostById: build.query<Post, number>({
       query: (postId) => `post/${postId}`
-    })
+    }),
+    // getPostById: build.query<Post, Post>({
+    //   query: (body: Post) => ({
+    //     url: `post/${body.id}`,
+    //     method: 'POST',
+    //     body,
+    //   })
+    // })
   }),
   overrideExisting: false,
 })
